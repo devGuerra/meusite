b54a55874984b0f21deff0ed647f19d2e6be1e48
Roberto Guerra <roberto.santa@semparar.com.br>
2020-05-20 21:36:26 -0300

    Updates

---
+++ precache-manifest.6b9af753ef52dad6e71f0e293b081eb1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "680a80acf7a08f737dd0b19f61f0235d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32338312b0869dc6cdc8",
+    "url": "/static/css/main.f58be416.chunk.css"
+  },
+  {
+    "revision": "236616bdb001e6484bae",
+    "url": "/static/js/2.5c8e6268.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5c8e6268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32338312b0869dc6cdc8",
+    "url": "/static/js/main.9f963804.chunk.js"
+  },
+  {
+    "revision": "45a8781ba23f52ef0b36",
+    "url": "/static/js/runtime-main.4e472fd2.js"
+  },
+  {
+    "revision": "ace81c4d599384e9959ba3cb0bdd6845",
+    "url": "/static/media/logowpp.ace81c4d.png"
+  },
+  {
+    "revision": "62354bd460ccd416fbbfe24710620696",
+    "url": "/static/media/newLogoColor.62354bd4.svg"
+  }
+]);
