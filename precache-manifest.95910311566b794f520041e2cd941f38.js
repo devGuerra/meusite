abc2ad777fad664efed7a7dca84b8adc9818800d
Roberto Guerra <roberto.santa@semparar.com.br>
2020-05-20 21:39:14 -0300

    Updates

---
+++ precache-manifest.95910311566b794f520041e2cd941f38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a7b90fc415944198fcca4903463114",
+    "url": "/meusite/index.html"
+  },
+  {
+    "revision": "32338312b0869dc6cdc8",
+    "url": "/meusite/static/css/main.f58be416.chunk.css"
+  },
+  {
+    "revision": "f1bd7f5d4463b72463bd",
+    "url": "/meusite/static/js/2.161a7f90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/meusite/static/js/2.161a7f90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32338312b0869dc6cdc8",
+    "url": "/meusite/static/js/main.9f963804.chunk.js"
+  },
+  {
+    "revision": "45b9470252b22da77654",
+    "url": "/meusite/static/js/runtime-main.87235ed1.js"
+  },
+  {
+    "revision": "ace81c4d599384e9959ba3cb0bdd6845",
+    "url": "/meusite/static/media/logowpp.ace81c4d.png"
+  },
+  {
+    "revision": "62354bd460ccd416fbbfe24710620696",
+    "url": "/meusite/static/media/newLogoColor.62354bd4.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b9af753ef52dad6e71f0e293b081eb1.js"
+  "/meusite/precache-manifest.95910311566b794f520041e2cd941f38.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/meusite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
